4b88753b9a61654d13a4293ea3ca4e9281b3a165
shuraa75 <harviann123@gmail.com>
2021-03-17 10:37:26 +0700

    Added Fragment to Fragment

--- O-Bako/app/src/main/java/com/example/o_bako/MainActivity.kt
+++ O-Bako/app/src/main/java/com/example/o_bako/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.o_bako
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.o_bako.fragments.InterfaceData
+import com.example.o_bako.fragments.JenisProduk
 import com.example.o_bako.fragments.MainHome
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity(), InterfaceData{
 
     var userLogin :String? = ""
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -22,5 +23,16 @@ class MainActivity : AppCompatActivity(){
         val fragment_home = MainHome.newInstance(userLogin.toString())
         supportFragmentManager
             .beginTransaction().replace(R.id.myContainer, fragment_home).commit()
+    }
+
+    override fun Kirim(user: String) {
+        val bundle = Bundle()
+        bundle.putString("Pesan", user)
+        val fragment_jenis = JenisProduk()
+        fragment_jenis.arguments = bundle
+        val transaksi = this.supportFragmentManager.beginTransaction()
+        transaksi.replace(R.id.myContainer, fragment_jenis)
+        transaksi.addToBackStack(null)
+        transaksi.commit()
     }
 }
---
+++ O-Bako/app/src/main/java/com/example/o_bako/fragments/JenisProduk.kt
@@ -0,0 +1,68 @@
+package com.example.o_bako.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import com.example.o_bako.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [JenisProduk.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class JenisProduk : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    var nama_jenis_barang : String? = ""
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_jenis_produk, container, false)
+
+        nama_jenis_barang = arguments?.getString("Pesan")
+        val namaTxt = view.findViewById<TextView>(R.id.nama_product)
+        namaTxt.text = nama_jenis_barang
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment JenisProduk.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            JenisProduk().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
