da2b9587715195c37e9c792bb1fd47c5fa2689d5
Chandra <ckesuma17@gmail.com>
2021-03-31 16:48:13 +0700

    aww

---
+++ O-Bako/app/src/main/java/com/example/o_bako/others/NotificationReceiver.kt
@@ -0,0 +1,25 @@
+package com.example.o_bako.others
+
+import android.app.AlarmManager
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.SystemClock
+import android.text.format.DateUtils
+import androidx.core.app.AlarmManagerCompat
+import androidx.core.content.ContextCompat
+
+class NotificationReceiver : BroadcastReceiver() {
+
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+        notificationManager.cancelAll()
+    }
+}
