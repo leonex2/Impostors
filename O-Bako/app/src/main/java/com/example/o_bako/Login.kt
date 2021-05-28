f35b1deedce22c2e0def43bb3804d4e07f511669
shuraa75 <harviann123@gmail.com>
2021-05-28 09:34:12 +0700

    Remake

--- O-Bako/app/src/main/java/com/example/o_bako/Login.kt
+++ O-Bako/app/src/main/java/com/example/o_bako/Login.kt
@@ -13,6 +13,8 @@ import androidx.room.Room
 import com.example.o_bako.Database.DBHelper
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
@@ -24,67 +26,88 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        var myLogin = input_login.text.toString()
-        readFileInternal()
+        var db = Room.databaseBuilder(
+                this,
+                DBHelper::class.java,
+                "obako.db"
+        ).build()
 
         btn_login.setOnClickListener {
-            if(input_login.text.toString() != "" && input_password.text.toString() != ""){
-                val intentToMainHome = Intent(this,MainActivity::class.java)
-                writeFileInternal()
-                input_login.setText("")
-                input_password.setText("")
-                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-                startActivity(intentToMainHome)
+            val intentToMainHome = Intent(this,MainActivity::class.java)
+            if (input_login.text.toString().isEmpty() && input_password.text.toString().isEmpty()){
+                Toast.makeText(this,"Data tolong diisi",Toast.LENGTH_SHORT).show()
             }
             else{
-                Toast.makeText(this, "Harap isi Username dan Password!", Toast.LENGTH_SHORT).show()
-            }
-        }
-        btn_signup.setOnClickListener{
-            val intentSignup = Intent(this,SignUp::class.java)
-            startActivityForResult(intentSignup, EXTRA_REQUEST_CODE)
-        }
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == EXTRA_REQUEST_CODE){
-            if(resultCode == EXTRA_RESULT_CODE){
-                input_login.setText (data?.getStringExtra(EXTRA_USERNAME) ?: "")
-                input_password.setText(data?.getStringExtra(EXTRA_PASSWORD) ?: "")
-            }
-            else if (resultCode == EXTRA_CANCEL_CODE){
-                input_login.setHint("Username")
-                input_password.setHint("Password")
-            }
-        }
-    }
-
-//Internal File Write&Read
-
-    private fun writeFileInternal() {
-        var output = openFileOutput("login.txt", Context.MODE_PRIVATE).apply {
-            write(input_login.text.toString().toByteArray())
-            close()
-        }
-        Toast.makeText(this,"File Save",Toast.LENGTH_SHORT).show()
-    }
-
-    private fun readFileInternal() {
-        input_login.text.clear()
-        try{
-            var input = openFileInput("login.txt").apply {
-                bufferedReader().useLines {
-                    for(text in it.toList()){
-                        input_login.setText("${input_login.text}$text")
+                var state = false
+                var myLogin = input_login.text.toString()
+                var myPass = input_password.text.toString()
+                doAsync {
+                    var userList = db.userDao().getAllData()
+                    for(allData in userList){
+                        if (myLogin == allData.username){
+                            if (myPass == allData.password){
+                                startActivity(intentToMainHome)
+                                state = true
+                                break
+                            }
+                        }
+                    }
+                    uiThread {
+                        if (state) {
+                            Toast.makeText(this@Login, "Login Sukses", Toast.LENGTH_SHORT)
+                                    .show()
+                        }
+                        else
+                            Toast.makeText(this@Login,"Username atau Password Salah",Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
-        catch (e : FileNotFoundException){
-            input_login.setHint("Username")
-        }
-        catch (e : IOException){
-            Toast.makeText(this,"File ERROR !",Toast.LENGTH_SHORT).show()
+        btn_signup.setOnClickListener{
+            val intentSignup = Intent(this,SignUp::class.java)
+            startActivity(intentSignup)
         }
     }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == EXTRA_REQUEST_CODE){
+//            if(resultCode == EXTRA_RESULT_CODE){
+//                input_login.setText (data?.getStringExtra(EXTRA_USERNAME) ?: "")
+//                input_password.setText(data?.getStringExtra(EXTRA_PASSWORD) ?: "")
+//            }
+//            else if (resultCode == EXTRA_CANCEL_CODE){
+//                input_login.setHint("Username")
+//                input_password.setHint("Password")
+//            }
+//        }
+//    }
+
+//Internal File Write&Read
+//
+//    private fun writeFileInternal() {
+//        var output = openFileOutput("login.txt", Context.MODE_PRIVATE).apply {
+//            write(input_login.text.toString().toByteArray())
+//            close()
+//        }
+//        Toast.makeText(this,"File Save",Toast.LENGTH_SHORT).show()
+//    }
+//
+//    private fun readFileInternal() {
+//        input_login.text.clear()
+//        try{
+//            var input = openFileInput("login.txt").apply {
+//                bufferedReader().useLines {
+//                    for(text in it.toList()){
+//                        input_login.setText("${input_login.text}$text")
+//                    }
+//                }
+//            }
+//        }
+//        catch (e : FileNotFoundException){
+//            input_login.setHint("Username")
+//        }
+//        catch (e : IOException){
+//            Toast.makeText(this,"File ERROR !",Toast.LENGTH_SHORT).show()
+//        }
+//    }
 }
--- O-Bako/app/src/main/java/com/example/o_bako/SignUp.kt
+++ O-Bako/app/src/main/java/com/example/o_bako/SignUp.kt
@@ -32,97 +32,85 @@ class SignUp : AppCompatActivity() {
             DBHelper::class.java,
             "obako.db"
         ).build()
+        btn_register.setOnClickListener {
+            val intentToLogin = Intent(this,Login::class.java)
+            var user_name = reg_name.text.toString()
+            var username = reg_username.text.toString()
+            var user_pw = reg_passwd.text.toString()
+            var user_homeaddress = reg_alamat.text.toString()
+            var user_email = reg_email.text.toString()
+            var user_phone = reg_phonenumber.text.toString()
+            var mySharedHelper = ShPrefHelper(this,PrefFileName)
 
-        if(isExternalStorageReadable()) {
-            btn_register.setOnClickListener {
-
-                var user_name = reg_name.text.toString()
-                var username = reg_username.text.toString()
-                var user_pw = reg_passwd.text.toString()
-                var user_homeaddress = reg_alamat.text.toString()
-                var user_email = reg_email.text.toString()
-                var user_phone = reg_phonenumber.text.toString()
-                var mySharedHelper = ShPrefHelper(this,PrefFileName)
-
-                var intent = Intent()
-                if (username.isEmpty() && user_pw.isEmpty()) {
-                    Toast.makeText(this, "Silahkan isi data dengan benar !", Toast.LENGTH_SHORT).show()
-                    setResult(EXTRA_CANCEL_CODE, intent)
+            var hasil = ""
+            doAsync {
+                db.userDao().insertUser(
+                    User(id = 0,user_name,username,user_pw,
+                        user_email,user_homeaddress,user_phone)
+                )
+                for(allData in db.userDao().getAllData()){
+                    hasil += "${allData.nama_user} ${allData.username} ${allData.password}" +
+                            "${allData.email} ${allData.alamat} ${allData.nomor_hp}"
                 }
-                else {
-                    writeFileExternal()
-                    var hasil = ""
-                    doAsync {
-                        db.userDao().insertUser(
-                            User(id = 0,user_name,username,user_pw,
-                                user_email,user_homeaddress,user_phone)
-                        )
-                        for(allData in db.userDao().getAllData()){
-                            hasil += "${allData.nama_user} ${allData.username} ${allData.password}" +
-                                    "${allData.email} ${allData.alamat} ${allData.nomor_hp}"
-                        }
 
-                    }
-                    mySharedHelper.nama = user_name
-                    mySharedHelper.username = username
-                    mySharedHelper.password = user_pw
-                    mySharedHelper.home_address = user_homeaddress
-                    mySharedHelper.email = user_email
-                    mySharedHelper.phone_number = user_phone
-
-                    intent.putExtra(EXTRA_USERNAME, username)
-                    intent.putExtra(EXTRA_PASSWORD, user_pw)
-                    Toast.makeText(this,"Akun Berhasil dibuat!", Toast.LENGTH_SHORT).show()
-                    setResult(EXTRA_RESULT_CODE, intent)
-                }
-                finish()
             }
+            mySharedHelper.nama = user_name
+            mySharedHelper.username = username
+            mySharedHelper.password = user_pw
+            mySharedHelper.home_address = user_homeaddress
+            mySharedHelper.email = user_email
+            mySharedHelper.phone_number = user_phone
+            intentToLogin.putExtra(EXTRA_USERNAME, username)
+            intentToLogin.putExtra(EXTRA_PASSWORD, user_pw)
+            Toast.makeText(this,"Akun Berhasil dibuat!", Toast.LENGTH_SHORT).show()
+            startActivity(intentToLogin)
+
         }
     }
 //    External File Write&Read
-    private fun writeFileExternal() {
-        var myDir = File(getExternalFilesDir(""),"MyFolder")
-        if(!myDir.exists()){
-            myDir.mkdir()
-        }
-        File(myDir,"Sign Up.txt").apply {
-            writeText(reg_username.text.toString())
-        }
-    }
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun isExternalStorageReadable(): Boolean{
-        if(ContextCompat.checkSelfPermission( //Untuk mengecek Permission yang telah diberikan Aplikasi
-                        this,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE //Permission yang diminta pada AndroidManifest
-                ) != PackageManager.PERMISSION_GRANTED){ //Apakah sudah Permission telah diberikan
-            requestPermissions( // Jika belum, maka akan diminta Permission untuk Read , Write pada External.
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                    123)
-        }
-        //Mengecek State dari External Storage
-        var state = Environment.getExternalStorageState()
-        //Apakah sdcard sudah terpasang ? atau sdcard hanya bisa di read
-        if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
-        {
-            return true
-        }
-        return false
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) { // Request Permission dalam Aplikasi yang akan mengembalikan sebuah Request Code
-        // Request code akan dikembalikan ke Fungsi isExternalStorageReadable() jika Access diberikan
-        when (requestCode) {
-            123 -> {
-                if ((grantResults.isNotEmpty() &&
-                                grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                ) {
-                    Toast.makeText(this, "Allowed !", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
+//    private fun writeFileExternal() {
+//        var myDir = File(getExternalFilesDir(""),"MyFolder")
+//        if(!myDir.exists()){
+//            myDir.mkdir()
+//        }
+//        File(myDir,"Sign Up.txt").apply {
+//            writeText(reg_username.text.toString())
+//        }
+//    }
+//    @RequiresApi(Build.VERSION_CODES.M)
+//    fun isExternalStorageReadable(): Boolean{
+//        if(ContextCompat.checkSelfPermission( //Untuk mengecek Permission yang telah diberikan Aplikasi
+//                        this,
+//                        Manifest.permission.WRITE_EXTERNAL_STORAGE //Permission yang diminta pada AndroidManifest
+//                ) != PackageManager.PERMISSION_GRANTED){ //Apakah sudah Permission telah diberikan
+//            requestPermissions( // Jika belum, maka akan diminta Permission untuk Read , Write pada External.
+//                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE),
+//                    123)
+//        }
+//        //Mengecek State dari External Storage
+//        var state = Environment.getExternalStorageState()
+//        //Apakah sdcard sudah terpasang ? atau sdcard hanya bisa di read
+//        if (Environment.MEDIA_MOUNTED.equals(state) || Environment.MEDIA_MOUNTED_READ_ONLY.equals(state))
+//        {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//            requestCode: Int,
+//            permissions: Array<out String>,
+//            grantResults: IntArray
+//    ) { // Request Permission dalam Aplikasi yang akan mengembalikan sebuah Request Code
+//        // Request code akan dikembalikan ke Fungsi isExternalStorageReadable() jika Access diberikan
+//        when (requestCode) {
+//            123 -> {
+//                if ((grantResults.isNotEmpty() &&
+//                                grantResults[0] == PackageManager.PERMISSION_GRANTED)
+//                ) {
+//                    Toast.makeText(this, "Allowed !", Toast.LENGTH_SHORT).show()
+//                }
+//            }
+//        }
+//    }
 }
