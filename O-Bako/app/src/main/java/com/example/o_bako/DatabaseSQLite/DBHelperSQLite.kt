1f24b519fbaad4268dd3fc51617d4728bf3235a3
shuraa75 <harviann123@gmail.com>
2021-06-03 07:32:04 +0700

    Insert, Create Done Read Incoming

--- O-Bako/app/src/main/java/com/example/o_bako/DatabaseSQLite/DBHelperSQLite.kt
+++ O-Bako/app/src/main/java/com/example/o_bako/DatabaseSQLite/DBHelperSQLite.kt
@@ -10,12 +10,8 @@ import android.database.sqlite.SQLiteOpenHelper
 class DBHelperSQLite(context : Context) :SQLiteOpenHelper(
         context, DATABASE_NAME,null, DB_VERSION) {
     companion object {
-        private val DB_VERSION = 1
-<<<<<<< HEAD
-        private val DATABASE_NAME = "test.db"
-=======
-        private val DATABASE_NAME = "o-bakosqllite.db"
->>>>>>> 488eee0467fce7c4ff76331531b2b08483b9bbdb
+        private val DB_VERSION = 2
+        private val DATABASE_NAME = "obako.db"
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
@@ -40,7 +36,7 @@ class DBHelperSQLite(context : Context) :SQLiteOpenHelper(
         val contentValues = ContentValues().apply {
             put(barangDB.tableBarang.COLUMN_NAMA, data.Nama)
             put(barangDB.tableBarang.COLUMN_DESKRIPSI, data.Deskripsi)
-            put(barangDB.tableBarang.COLUMN_QTY, data.Qty)
+            put(barangDB.tableBarang.COLUMN_QTY,data.Qty)
             put(barangDB.tableBarang.COLUMN_HARGA, data.Harga_Barang)
         }
         val success = db.insert(
@@ -54,7 +50,7 @@ class DBHelperSQLite(context : Context) :SQLiteOpenHelper(
     fun getBarang(): MutableList<Data> {
         val dataList : MutableList<Data> = mutableListOf()
         val query = "SELECT * FROM" +
-                "${barangDB.tableBarang.TABLE_BARANG}"
+                " ${barangDB.tableBarang.TABLE_BARANG}"
         val db = this.readableDatabase
         var cursor: Cursor? = null
         try {
